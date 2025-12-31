7046d221cab5016b1810112ab8f127af95721ef3
tirucloud <tirucloud@gmail.com>
2025-12-31 08:36:48 +0530

    all

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-backend-tws-hackathon"
+    bucket = "tiru-12348901"
     key    = "backend-locking"
     region = "ap-south-1"
     use_lockfile = true
